cd3cff8afbc935d2bc8c5d2638085db1db9939dd
mohanadqasim <mohanad.qasim@gotmail.com>
2023-02-28 12:46:56 +0000

    labDone

---
+++ schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS allmovies;
+
+CREATE TABLE IF NOT EXISTS allmovies (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    release_date VARCHAR(255),
+    poster_path VARCHAR(1000),
+    overview VARCHAR(10000)
+);
