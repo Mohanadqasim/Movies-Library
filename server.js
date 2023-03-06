f9f598d3077e3841c69dc0085044b1b95ed40a52
mohanadqasim <mohanad.qasim@gotmail.com>
2023-03-06 08:45:10 +0000

    deployment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 server.use(express.json());
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const client = new pg.Client(process.env.DATABASE_URL);
 
 
